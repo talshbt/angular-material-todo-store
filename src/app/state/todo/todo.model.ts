5d460e801cfc50f5572c10f1b2bec4c29bfe4df7
talshbt <talshbt@gmail.com>
2021-06-09 11:59:22 +0300

    1 file modified

--- src/app/state/todo/todo.model.ts
+++ src/app/state/todo/todo.model.ts
@@ -8,13 +8,9 @@ export function generateToDos(): ToDo[] {
   return [
     {
       id: 1,
-      task: 'Watch Ozark Season 2',
+      task: '172.19.15.49',
       complete: false
-    },
-    {
-      id: 2,
-      task: 'Use NgRx in my to-do app',
-      complete: true
     }
+
   ];
 }
